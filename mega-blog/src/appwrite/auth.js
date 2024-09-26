3ae7fd8dfd0f4071655ec77fc72c806f2a5eee7c
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-09-27 00:25:04 +0530

    Appwrite Authentication service implemented

---
+++ mega-blog/src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import conf from "../conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client =  new Client();
+  account;
+
+  constructor(){
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({email, password, name}){
+
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        // another method to get logged in
+        this.login({email,password});
+        
+      } else {
+        return userAccount;
+        
+      }
+
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
+   async login({email,password}){
+        try {
+
+          const session = await account.createEmailPasswordSession(email, password);
+          return session
+          
+        } catch (error) {
+          throw error
+        }
+  }
+
+   async getCurrentUser(){
+    try {
+    const user = await account.get();
+    return user
+    // Logged in
+      } catch (err) {
+    // Not logged in
+        throw err
+      }
+      return null;
+
+   }
+
+   async logout(){
+    try {
+      await this.account.deleteSessions()
+      
+    } catch (error) {
+      throw error
+      
+    }
+   }
+
+}
+
+const authservice = new AuthService();
+
+export default authservice
