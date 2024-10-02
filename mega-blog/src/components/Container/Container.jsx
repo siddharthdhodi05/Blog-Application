782637c364022204bfcfaee01514ad5d30657e94
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-10-02 20:58:22 +0530

    Made components like input, loginbtn,container etc

---
+++ mega-blog/src/components/Container/Container.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+
+function Container() {
+  return (
+    <div className=' w-full max-w-7xl mx-auto px-4'>
+      {children}
+    </div>
+  )
+}
+
+export default Container
--- mega-blog/src/components/index.js
+++ mega-blog/src/components/index.js
@@ -1,4 +1,8 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export {Footer, Header}
+
+export {Footer, Header, Container, Logo, LogoutBtn}
