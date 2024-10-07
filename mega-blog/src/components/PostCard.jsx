833abad896e593b8d14ee279d1ae915ca9792ddd
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-10-07 21:48:44 +0530

    postcard and select componets implementation

---
+++ mega-blog/src/components/PostCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import appwriteService from '../appwrite/config'
+import { Link } from 'react-router-dom'
+
+function PostCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostCard
